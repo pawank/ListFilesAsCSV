da2ebae7643402cf472212a2d0e2fefcac806ceb
Pawan Kumar <pawan.kumar@gmail.com>
2015-12-27 19:26:50 +0530

    Filter files based on ext provided on the cmd line

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -7,10 +7,21 @@ import ammonite.ops._
 
 
 object Program {
+
+  def listOfFiles(path:String, extensions:Seq[String]=Seq.empty):Seq[Path] = {
+    //val wd = cwd/s"${path}"
+    val wd:Path = Path(path)
+    val listed = extensions.isEmpty match {
+      case true => ls.rec! wd
+      case false => ls.rec! wd |? (f => extensions.contains(f.ext))
+    }
+    listed.foreach(println)
+    listed
+  }
+
   def main(args:Array[String]) = {
-    val wd = cwd/s"${args(0)}"
-    val listed = ls! wd
-    println(listed)
+    listOfFiles(args(0),args.tail.toSeq)
+    print("")
   }
 }
 
