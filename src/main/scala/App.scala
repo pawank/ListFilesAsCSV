fe02b18975ce89233b00b037e14327341581286e
Pawan Kumar <pawan.kumar@gmail.com>
2015-12-27 19:46:06 +0530

    Comments added for usage

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -5,21 +5,29 @@ package apps
 
 import ammonite.ops._
 
-
+//Examples of usage
+//<Program> /dummy/path:target jpg png
+//<Program> /full/path/of/the/target
 object Program {
 
   def listOfFiles(path:String, extensions:Seq[String]=Seq.empty):Seq[Path] = {
     //val wd = cwd/s"${path}"
+
     val (prefix,fullPath) = path.indexOf(":") >= 0 match {
       case true => (path.split(":").head,path.split(":").tail.mkString("/"))
       case _ => ("",path)
     }
-    val wd:Path = Path(fullPath)
+    val cwdPath = cwd.toString()
+    val isAbsPath = fullPath.startsWith("/")
+    val wd:Path = if (isAbsPath) Path(fullPath) else cwd/s"${fullPath}"
     val listed = extensions.isEmpty match {
       case true => ls.rec! wd
       case false => ls.rec! wd |? (f => extensions.contains(f.ext))
     }
-    if (prefix.isEmpty) listed else listed.map(path => Path(prefix + path.toString()))
+
+    def makeCorrectPath(path:String):String = if (isAbsPath) path else path.replaceAll(cwdPath,"")
+
+    if (prefix.isEmpty) listed.map(path => Path(makeCorrectPath(path.toString()))) else listed.map(path => Path(makeCorrectPath(prefix + path.toString())))
   }
 
   def main(args:Array[String]) = {
