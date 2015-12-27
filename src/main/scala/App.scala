e70cc431f51be5f0d22dc96046283968e8a23346
Pawan Kumar <pawan.kumar@gmail.com>
2015-12-27 19:37:38 +0530

    Use prefix to add to the start of the path

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -10,17 +10,21 @@ object Program {
 
   def listOfFiles(path:String, extensions:Seq[String]=Seq.empty):Seq[Path] = {
     //val wd = cwd/s"${path}"
-    val wd:Path = Path(path)
+    val (prefix,fullPath) = path.indexOf(":") >= 0 match {
+      case true => (path.split(":").head,path.split(":").tail.mkString("/"))
+      case _ => ("",path)
+    }
+    val wd:Path = Path(fullPath)
     val listed = extensions.isEmpty match {
       case true => ls.rec! wd
       case false => ls.rec! wd |? (f => extensions.contains(f.ext))
     }
-    listed.foreach(println)
-    listed
+    if (prefix.isEmpty) listed else listed.map(path => Path(prefix + path.toString()))
   }
 
   def main(args:Array[String]) = {
-    listOfFiles(args(0),args.tail.toSeq)
+    val paths = listOfFiles(args(0),args.tail.toSeq)
+    paths.foreach(println)
     print("")
   }
 }
